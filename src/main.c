8b0e86d29937a857df65a56cf781c671662c36ef
FluxoDesign <infinixion@gmail.com>
2017-09-12 21:35:43 +1000

    Commited project files

---
+++ src/main.c
@@ -0,0 +1,33 @@
+/**
+  ******************************************************************************
+  * @file    main.c
+  * @author  Ac6
+  * @version V1.0
+  * @date    01-December-2013
+  * @brief   Default main function.
+  ******************************************************************************
+*/
+
+
+#include "stm32f0xx.h"
+#include "stm32f0_discovery.h"
+
+GPIO_InitTypeDef gpio;
+
+// LEDS
+#define GREEN_LED GPIO_Pin_9
+#define BLUE_LED GPIO_Pin_8
+#define LED_GPIO GPIOC
+
+
+int main(void){
+	RCC_AHBPeriphClockCmd(RCC_AHBPeriph_GPIOA, ENABLE);
+	RCC_AHBPeriphClockCmd(RCC_AHBPeriph_GPIOC, ENABLE);
+
+	gpio.GPIO_Pin = GREEN_LED | BLUE_LED;
+	gpio.GPIO_Mode = GPIO_Mode_OUT;
+	gpio.GPIO_OType = GPIO_OType_PP;
+	gpio.GPIO_PuPd = GPIO_PuPd_NOPULL;
+	gpio.GPIO_Speed = GPIO_Speed_Level_1;
+	GPIO_Init(LED_GPIO, &gpio);
+}
