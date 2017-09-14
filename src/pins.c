445a0fbea35e6e0c1731fd112e1e9c954232dafc
Ronald Kurniawan <r.kurniawan@fluxodesign.net>
2017-09-14 23:08:25 +1000

    * src/pins.c: stub for pin utilities function file * src/pins.h: header stub for pin utilities function file

---
+++ src/pins.c
@@ -0,0 +1,9 @@
+/*
+ * pins.c
+ *
+ *  Created on: 14 Sep 2017
+ *      Author: viper
+ */
+
+#include "pins.h"
+
---
+++ src/pins.h
@@ -0,0 +1,13 @@
+/*
+ * pins.h
+ *
+ *  Created on: 14 Sep 2017
+ *      Author: viper
+ */
+
+#ifndef PINS_H_
+#define PINS_H_
+
+
+
+#endif /* PINS_H_ */
